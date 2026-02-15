392d7aceb37b80a8c7f08f32351d150abac6a539
z-mroku <krzgod8003@gmail.com>
2026-02-15 03:39:07 +0100

    Update sekcja-loader.js

--- js/sekcja-loader.js
+++ js/sekcja-loader.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
 // --- Inicjalizacja Aplikacji ---
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const storage = getStorage(app);
+const storage = getStorage(app, "gs://projekt-latarnia.firebasestorage.app");
 
 // ==========================================================
 // 🎵 CONFIG HYMNU 🎵
